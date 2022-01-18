eca90faa750e6e3dace744bf56dbd70b10b1f534
HelloDev0 <erdpdash95@gmail.com>
2022-01-18 10:33:47 +0530

    login Api created

---
+++ WebApp/src/api/login/controllers/login.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * A set of functions called "actions" for `login`
+ */
+
+module.exports = 
+{
+  exampleAction: async (ctx, next) => {
+    console.log(ctx)
+    try {
+
+      ctx.body = 'ok';
+    } catch (err) {
+      ctx.body = err;
+    }
+  }
+};
--- WebApp/src/api/proctoring-user/routes/proctoring-user.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * proctoring-user router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::proctoring-user.proctoring-user');
